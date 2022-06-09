c5ad98837f64070c5de9c1a91c1a11f785199785
Jeremy <jeremy__francis@hotmail.com>
2022-06-09 12:38:15 -0700

    commit tree 910761c8613e047ea5344d237c25cb83fc917ed1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    return customerName = customerName.toLocaleUpperCase()
+}
+function setBestCustomer(){
+   return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+ const leastFavoriteCustomer = 'Sally'
+
+ function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Sally'
+}
